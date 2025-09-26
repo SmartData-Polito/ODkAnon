b0fb6b3546dd5fa61293c84f4984158babcbf51e
Kai HUANG <kai.huang@polito.it>
2025-09-26 08:50:14 +0000

    mondrian finished

---
+++ utils/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import h3
+
+def filter_by_h3_hexagons(df, target_hexagons, resolution=6):
+    """
+    Filter the dataset, keeping only the rows where both start and end
+    are located in the specified H3 hexagons.
+    
+    Parameters:
+    - df: DataFrame with columns ori_lat, ori_lon, dst_lat, dst_lon	
+    - target_hexagons: set of target H3 hexagons
+    - resolution: H3 resolution (default 6, based on target hexagons)
+    
+    Returns:
+    - Filtered DataFrame
+    """
+    
+    # map latitude and longitude to h3 hexagon
+    df['ori_h3'] = df.apply(lambda row: h3.latlng_to_cell(row['ori_lat'], row['ori_lon'], resolution), axis=1)
+    df['dst_h3'] = df.apply(lambda row: h3.latlng_to_cell(row['dst_lat'], row['dst_lon'], resolution), axis=1)
+    
+    filtered_df = df[
+        (df['ori_h3'].isin(target_hexagons)) & 
+        (df['dst_h3'].isin(target_hexagons))
+    ].copy()
+    
+    return filtered_df
